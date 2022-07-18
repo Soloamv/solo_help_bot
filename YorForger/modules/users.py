e01d9c27da7e7df953fc31d94724e209e94046f7
Iris <97659548+Aera-02@users.noreply.github.com>
2022-07-18 15:44:48 +0530

    Update users.py

--- YorForger/modules/users.py
+++ YorForger/modules/users.py
@@ -120,7 +120,7 @@ def __user_info__(user_id):
 
 
 def __stats__():
-    return "× {} users, across {} chats".format(sql.num_users(), sql.num_chats())
+    return f"• {sql.num_users()} users, across {sql.num_chats()} chats"
 
 
 def __migrate__(old_chat_id, new_chat_id):
